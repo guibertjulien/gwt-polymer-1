fcfef78131dfafe8aae7ae0238b1733ccc689b36
Rodrigo Pecci Acorse <rodrigopeccirpa@gmail.com>
2014-12-05 00:09:47 -0200

    Updating project structure & Upgrading polymer classes

--- src/main/java/br/com/pecci/acorse/client/coreelements/CoreSelectorItemRenderer.java
+++
@@ -1,9 +0,0 @@
-package br.com.pecci.acorse.client.coreelements;
-
-import com.google.gwt.dom.client.Element;
-
-public abstract class CoreSelectorItemRenderer<T> {
-
-	public abstract Element render(T item);
-
-}
---
+++ src/main/java/br/com/rpa/client/App.java
@@ -0,0 +1,45 @@
+package br.com.rpa.client;
+
+import br.com.rpa.client.paperelements.PaperButton;
+import br.com.rpa.client.ui.PolymerTest;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class App implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	@Override
+	public void onModuleLoad() {
+		// Creating a paper button.
+		PaperButton button = new PaperButton();
+		button.setLabel("Botão");
+		button.setRaisedButton(true);
+
+		// Getting the value of a paper button property.
+		System.out.println(button.isRaisedButton());
+
+		// Adding a event handler to the paper button.
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("Teste!!");
+			}
+		});
+
+		RootPanel.get().add(button);
+
+		// Creating uiBinder for polymer elements
+		PolymerTest uiPolymer = new PolymerTest("Teste");
+
+		RootPanel.get().add(uiPolymer);
+	}
+}
